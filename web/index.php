104f75e8b4957abee81e70a4d6e8315f058a924f
Adrien Gallou <adriengallou@gmail.com>
2014-07-02 21:28:13 +0200

    affichage des derniers issues soumises et fermées

--- web/index.php
+++ web/index.php
@@ -6,10 +6,11 @@ $app = new Silex\Application();
 
 $issuesIds = array(
   'symfony/symfony/issues/5',
+  'symfony/symfony/issues/6',
+  'symfony/symfony/issues/7',
+  'symfony/symfony/issues/8',
+  'symfony/symfony/issues/11263',
 );
-
-$app['debug'] = true;
-
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
@@ -17,18 +18,41 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $client = new GuzzleHttp\Client(array('base_url' => 'https://api.github.com'));
 
-$issues = array();
+$app->get('/', function() use($app, $client, $issuesIds) {
 
-foreach ($issuesIds as $issue) {
-  $issues[$issue] = $client->get('repos/' . $issue)->json();
-}
+    $issues = array();
 
-$app->get('/', function() use($app, $issues) {
-  return $app['twig']->render('index.twig', array(
-    'issues' => $issues,
-  ));
-//  $issue = $res->json();
-//  return $issue['title'];
+    foreach ($issuesIds as $issuePath) {
+        $issues[$issuePath] = $client->get('repos/' . $issuePath)->json();
+    }
+
+    $sortByClosedAt = function($issueA, $issueB) {
+        return $issueA['closed_at'] < $issueB['closed_at'];
+    };
+
+    $sortByCreatedAt = function($issueA, $issueB) {
+        return $issueA['created_at'] < $issueB['created_at'];
+    };
+
+    $closedIssues = array();
+
+    foreach ($issues as $issueId => $issue) {
+
+        if ($issue['state'] != 'closed') {
+            continue;
+        }
+        $closedIssues[$issueId] =  $issue;
+    }
+
+    uasort($issues, $sortByCreatedAt);
+    uasort($closedIssues, $sortByClosedAt);
+
+    return $app['twig']->render('index.twig', array(
+      'issues' => $issues,
+      'closed_issues' => $closedIssues,
+      'last_issues'        => array_slice($issues, 0, 5),
+      'last_closed_issues' => array_slice($closedIssues, 0, 5),
+    ));
 });
 
 $app->run();
