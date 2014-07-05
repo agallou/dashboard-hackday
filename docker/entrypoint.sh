28e0d32bbc767562d51892af47171f80bb83b5d0
Geoffrey Bachelet <ubermuda@users.noreply.github.com>
2014-07-05 14:21:43 +0200

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,8 +5,20 @@ echo 'No application found in /var/www'
 exit 1;
 fi
 
+
+
 if [ ! -z "$GITHUB_TOKEN" ]; then
     echo "env[GITHUB_TOKEN] = '`echo $GITHUB_TOKEN`'" >> /etc/php5/fpm/php-fpm.conf;
+    
+    cat > /.composer/config.json <<EOF
+{
+    "config": {
+        "github-oauth": {
+            "github.com": "$GITHUB_TOKEN"
+        }
+    }
+}
+EOF
 fi
 
 if [ ! -d vendor ]; then
