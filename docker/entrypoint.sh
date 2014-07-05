abfa6dec7c291cdb37231ce9d06a00181c605d81
Geoffrey Bachelet <ubermuda@users.noreply.github.com>
2014-07-05 14:10:23 +0200

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,8 +5,12 @@ echo 'No application found in /var/www'
 exit 1;
 fi
 
+if [ ! -z "$GITHUB_TOKEN" ]; then
+    echo "env[GITHUB_TOKEN] = '`echo $GITHUB_TOKEN`'" >> /etc/php5/fpm/php-fpm.conf;
+fi
+
 if [ ! -d vendor ]; then
     composer install
 fi
- 
+
 exec svscan /srv/services
